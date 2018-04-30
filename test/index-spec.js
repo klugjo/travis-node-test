312a75fe5425efd23a6afae6ac7cafcc0347a408
Leonardo Furnielis <leonardo.furnielis1@ibm.com>
2018-04-30 14:28:30 -0300

    up

--- test/index-spec.js
+++ test/index-spec.js
@@ -5,7 +5,7 @@ describe('calculator', function() {
     describe('add function', function() {
         it('adds numbers', function () {
             var result = calculator.add(1, 1);
-            assert.equal(result, 2);
+            assert.equal(result, 3);
         });
     });
 });
